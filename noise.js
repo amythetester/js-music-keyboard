8d9045e2eedec5d7b73156bc6af676c08edea992
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-28 21:24:02 -0700

    refactored switch statement

--- noise.js
+++ noise.js
@@ -1,35 +1,8 @@
 $(document).ready( function() {
   $('body').keydown(function(event) {
-    const aAudio = document.getElementById("aAudio");
-    const bAudio = document.getElementById("bAudio");
-    const cAudio = document.getElementById("cAudio");
-    const dAudio = document.getElementById("dAudio");
-    const eAudio = document.getElementById("eAudio");
-    const fAudio = document.getElementById("fAudio");
-    const gAudio = document.getElementById("gAudio");
-
-    switch(event.key) {
-      case 'a': 
-        aAudio.play();
-        break;
-      case 'b':
-        bAudio.play();
-        break;
-      case 'c':        
-        cAudio.play();
-        break;
-      case 'd':        
-        dAudio.play();
-        break;
-      case 'e':        
-        eAudio.play();
-        break;
-      case 'f':        
-        fAudio.play();
-        break;
-      case 'g':        
-        gAudio.play();
-    }
+    const audioKey = `${event.key}Audio`
+    const audio = document.getElementById(audioKey);
+    audio.play();
   });
 });
 
