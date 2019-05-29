ee8f7c2f8b87dc8a3608750af42239e36d85b543
Amy Wyatt <amy.wyatt12@outlook.com>
2019-05-28 21:03:56 -0700

    key strokes create musical sound

--- noise.js
+++ noise.js
@@ -1,3 +1,37 @@
 $(document).ready( function() {
-  // your code here
+  $('body').keydown(function(event) {
+    if (event.key === 'a') {
+      const aAudio = document.getElementById("aAudio");
+      aAudio.play();
+    }
+    if (event.key === 'b') {
+      const aAudio = document.getElementById("bAudio");
+      aAudio.play();
+    }
+    if (event.key === 'c') {
+      const aAudio = document.getElementById("cAudio");
+      aAudio.play();
+    }
+    if (event.key === 'd') {
+      const aAudio = document.getElementById("dAudio");
+      aAudio.play();
+    }
+    if (event.key === 'e') {
+      const aAudio = document.getElementById("eAudio");
+      aAudio.play();
+    }
+    if (event.key === 'f') {
+      const aAudio = document.getElementById("fAudio");
+      aAudio.play();
+    }
+    if (event.key === 'g') {
+      const aAudio = document.getElementById("gAudio");
+      aAudio.play();
+    }
+  })
 });
+
+
+
+
+
